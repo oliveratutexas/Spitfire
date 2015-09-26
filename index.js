83866b5280d347e90a1ff46b2b4faec72593149d
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2015-09-26 16:28:11 -0500

    Add css and put words on page

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const rhymes = require('rhymes');
 
-console.log(rhymes('word'));
+var suggestionsNode = document.querySelector('.suggestions');
+var firstWordNode = suggestionsNode.querySelector('.first');
+var secondWordNode = suggestionsNode.querySelector('.second');
+var thirdWordMode = suggestionsNode.querySelector('.third');
+
+var updateSuggestions = function(arr) {
+    firstWordNode.textContent = arr[0];
+    secondWordNode.textContent = arr[1];
+    thirdWordMode.textContent = arr[2];
+};
+
+var words = rhymes('word').map(function(obj) {
+    return obj.word;
+}).slice(0,3);
+
+updateSuggestions(words);
