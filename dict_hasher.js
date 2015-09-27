d97ce91892855333b2b567f8aa57e7d8cba4b860
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2015-09-27 00:52:57 -0500

    Add ~4280 word precomputed dict

--- dict_hasher.js
+++ dict_hasher.js
@@ -1,67 +1,59 @@
-const cmu_dict = require('cmu-pronouncing-dictionary');
 const merge = require('lodash').merge
 const sortBy = require('lodash').sortBy
 const fs = require('fs');
 
-const map = new Map();
-var words = [];
+our_cmu_dict = JSON.parse(fs.readFileSync('our_cmu_dict').toString());
 
-Object.keys(cmu_dict).forEach(function (word) {
-  words.push({
+const map = {};
+
+var ocd = Object.keys(our_cmu_dict).map(function(word) {
+  return {
     word: word,
-    pron: cmu_dict[word]
-  })
+    pron: our_cmu_dict[word]['pronunciation'],
+    partOfSpeech: our_cmu_dict[word]['partOfSpeech']
+  };
 });
 
 var countMatchingTrailingSyllablesInPronunciations = function(a, b) {
-  a = a.split(' ').reverse()
-  b = b.split(' ').reverse()
-  var score = 0
-  var shorterPron = (a.length < b.length) ? a : b
+    a = a.split(' ').reverse()
+    b = b.split(' ').reverse()
+    var score = 0
+    var shorterPron = (a.length < b.length) ? a : b
 
-  for (var i in shorterPron) {
-    if (a[i] === b[i]) {
-      score++
-    } else {
-      return score
+    for (var i in shorterPron) {
+      if (a[i] === b[i]) {
+        score++
+      } else {
+        return score
+      }
     }
-  }
 
-  return score
+    return score
 };
 
-words.forEach(function(obj) {
+ocd.forEach(function(obj) {
     var input = obj.word;
-
-    var results = [];
     console.log('working on input', input);
+    if (!input) return []
 
     input = input.toLowerCase()
-    
-    if (!cmu_dict[input]) {
-        map.add(input, results);
-        return;
+
+    var inputPron = obj.pron;
+    var results = [];
+      
+    for (var i = 0; i < ocd.length; i += 1) {
+        var obj2 = ocd[i];
+        
+        var score = countMatchingTrailingSyllablesInPronunciations(inputPron, obj2.pron)
+        if (score > 1) {
+            // console.log(results)
+            results.push(merge(obj2, {score: score}))
+        }
     }
-    
-    var inputPron = cmu_dict[input]
 
-    words.forEach(function(word) {
-      if (word.pron == null) {
-        return;
-      }
-
-      var score = countMatchingTrailingSyllablesInPronunciations(inputPron, word.pron)
-      if (score > 1) {
-        results.push(merge(word, {score: score}))
-      }
-      results = sortBy(results, 'score')
-        .reverse()
-        .slice(0, 20);
-    });
-
-    // console.log('setting results', results);
-    map.set(input, results);
+    results = sortBy(results, 'score').reverse()
+    map[input] = results;
 });
 
-console.log(map.entires().length);
 fs.writeFileSync('map', JSON.stringify(map));
+console.log(JSON.parse(fs.readFileSync('map').toString()));
